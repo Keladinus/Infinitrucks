f5df7029ec6990532204ca2370bc315ad310d059
JamieCCH <jamie.cch@gmail.com>
2017-07-11 01:08:07 -0400

    Add cheat code for HUD
    I / K: +/- health
    J / L: +/- score

--- Scripts/Core/ui.js
+++ Scripts/Core/ui.js
@@ -1,31 +1,20 @@
 var c = document.getElementById("hud");
 var ctx = c.getContext("2d");
 
-// Create gradient
-//var grd = ctx.createLinearGradient(0,0,200,0);
-//grd.addColorStop(0,"red");
-//grd.addColorStop(1,"white");
-
-// Fill with gradient
-//ctx.fillStyle = grd;
-//ctx.fillRect(10,10,150,80);
-
-//----------
-
 var spriteObject =
 {
   //The x and y source position of the sprite's image
   //And its height and width
   sourceX: 0,
   sourceY: 0,
-  sourceWidth: 245,
+  sourceWidth: 240,
   sourceHeight: 75,
   
   //The x and y position of the sprite on the canvas
   //as well as its height
   x: 0,
   y: 0,
-  width: 245,
+  width: 240,
   height: 75,
 };
 
@@ -39,7 +28,7 @@ sprites.push(outerMeter);
 
 //Create the inner health meter 
 var innerMeter = Object.create(spriteObject);
-innerMeter.sourceY = 85;
+innerMeter.sourceY = 84.3;
 innerMeter.x = outerMeter.x;
 innerMeter.y = outerMeter.y;
 sprites.push(innerMeter);
@@ -54,8 +43,8 @@ sprites.push(outerScore);
 var innerScore = Object.create(spriteObject);
 innerScore.sourceY = 171;
 innerScore.x = outerScore.x;
-innerScore.width = 50;
-innerScore.sourceWidth = 50;
+innerScore.width = 56;
+innerScore.sourceWidth = 56;
 sprites.push(innerScore);
 
 //Load the image
@@ -65,10 +54,13 @@ image.src = "Assets/images/hudMeterSprites_HUD.png";
 
 function loadHandler()
 { 
-  update();
+  render(); 
+  uiUpdate();
 }
 
-function update() {
+function uiUpdate(){
+  window.addEventListener("keydown", onKeyDown);
+}
 
 /*
 if car and truk collision
@@ -80,24 +72,61 @@ innerScore.width++;
 innerScore.sourceWidth++;
 */
 
-if(function collisionCheck(player, trucks){}){
-    if(innerMeter.width > 0)
-    {
-      innerMeter.width--;
-      innerMeter.sourceWidth--;
-      console.log(innerMeter.width);
-    }
-  
-    if(innerMeter.width < 1)
-     {
-    //gameOver.display;
-    }
+function onKeyDown(event)
+{
+  switch(event.keyCode)
+	{
+  	case 73: // I key, to increase health
+            if(innerMeter.width < 240 && paused == false)
+            {
+              innerMeter.width += 10;
+              innerMeter.sourceWidth += 10;
+              render();
+              console.log("width = "+innerMeter.width);
+              console.log("sourceWidth = "+innerMeter.sourceWidth);
+            }
+            break;
+		case 75: // K key, to decrease health
+            if(innerMeter.width > 65 && paused == false)
+            {
+              innerMeter.width -=25;
+              innerMeter.sourceWidth -=25;
+              render();
+              console.log("width = "+innerMeter.width);
+              console.log("sourceWidth = "+innerMeter.sourceWidth);
+            }else if(innerMeter.width <=65 && paused == false)
+            {
+            //game over screen
+            }
+            break;
+    case 76: // J key, to increase score
+            if(innerScore.width < 232 && paused == false)
+            {
+              innerScore.width += 2;
+              innerScore.sourceWidth += 2;
+              render();
+              console.log("score width = "+innerScore.width);
+              console.log("score sourceWidth = "+innerScore.sourceWidth);
+            }
+            break;
+// we don't have the situation to decrease scores so far (maybe add in level 2 when hit the animals?)
+		case 74: // L key, to decrease score
+            if(innerScore.width > 56 && paused == false)
+            {
+              innerScore.width -=2;
+              innerScore.sourceWidth -=2;
+              render();
+              console.log("score width = "+innerScore.width);
+              console.log("score sourceWidth = "+innerScore.sourceWidth);
+            }
+            break;
+
+    default:
+				console.log("cheat code unhandled key.");
+				break;
+  }
 }
 
-render();
-
-    
-}
 
 function render() {
     ctx.clearRect(0, 0, c.width, c.height); 
@@ -114,6 +143,15 @@ function render() {
          //console.log(image);
         }
      }
-
-    
 }
+
+
+//-------------------
+//The start menu
+//-------------------
+
+var startCanvas = document.getElementById("start");
+var startCtx = startCanvas.getContext("2d");
+var startWidth = startCanvas.getAttribute('width');
+var startHeight = startCanvas.getAttribute('height');
+
